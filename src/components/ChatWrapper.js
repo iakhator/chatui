0d7340f5f17ed65a3ffe88fd440a3dc1a61c7bb3
iakhator <iakhator@gmail.com>
2021-07-01 14:48:25 +0100

    fix chat scroll and positioning

--- src/components/ChatWrapper.js
+++ src/components/ChatWrapper.js
@@ -3,47 +3,49 @@ import React from 'react';
 export default function ChatWrapper () {
   return(
     <>
-      <div className="chat__wrapper">
-        <div className="chat">
-          <div className="chat__messages">
-            <span>author</span>
-            <p>message</p>
-            <span className="chat__messages-date">date</span>
-          </div>
-          <div className="chat__messages">
-            <span>author</span>
-            <p>message</p>
-            <span className="chat__messages-date">date</span>
-          </div>
-          <div className="chat__messages">
-            <span>author</span>
-            <p>message</p>
-            <span className="chat__messages-date">date</span>
-          </div>
-          <div className="chat__messages">
-            <span>author</span>
-            <p>message</p>
-            <span className="chat__messages-date">date</span>
-          </div>
-          <div className="chat__messages">
-            <span>author</span>
-            <p>message</p>
-            <span className="chat__messages-date">date</span>
-          </div>
-          <div className="chat__messages">
-            <span>author</span>
-            <p>message</p>
-            <span className="chat__messages-date">date</span>
-          </div>
-          <div className="chat__messages">
-            <span>author</span>
-            <p>message</p>
-            <span className="chat__messages-date">date</span>
-          </div>
-          <div className="chat__messages user-chat">
-            <span>author</span>
-            <p>message</p>
-            <span className="chat__messages-date">date</span>
+     <div className="wrap">
+        <div className="chat__wrapper">
+          <div className="chat">
+            <div className="chat__messages">
+              <span>author</span>
+              <p>message</p>
+              <span className="chat__messages-date">date</span>
+            </div>
+            <div className="chat__messages">
+              <span>author</span>
+              <p>message</p>
+              <span className="chat__messages-date">date</span>
+            </div>
+            <div className="chat__messages">
+              <span>author</span>
+              <p>message</p>
+              <span className="chat__messages-date">date</span>
+            </div>
+            <div className="chat__messages">
+              <span>author</span>
+              <p>message</p>
+              <span className="chat__messages-date">date</span>
+            </div>
+            <div className="chat__messages">
+              <span>author</span>
+              <p>message</p>
+              <span className="chat__messages-date">date</span>
+            </div>
+            <div className="chat__messages">
+              <span>author</span>
+              <p>message</p>
+              <span className="chat__messages-date">date</span>
+            </div>
+            <div className="chat__messages">
+              <span>author</span>
+              <p>message</p>
+              <span className="chat__messages-date">date</span>
+            </div>
+            <div className="chat__messages user-chat">
+              <span>author</span>
+              <p>message</p>
+              <span className="chat__messages-date">date</span>
+            </div>
           </div>
         </div>
       </div>
